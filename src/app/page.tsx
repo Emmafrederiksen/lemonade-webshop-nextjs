1413cf94e941252d8668ebb5dcedc885e40aa91d
emmao <emf002@edu.zealand.dk>
2026-03-09 21:54:19 +0100

    Made Hero.tsx reuseable with props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Container from "../components/layout/Container";
 import Hero from "../components/sections/hero/Hero";
 import FeaturedProducts from "../components/sections/featured/FeaturedProducts";
 
@@ -7,8 +5,12 @@ export default function Home() {
   return (
     <main>
 
-      <Hero />
-      
+      <Hero
+        title="Fresh handmade lemonade"
+        description="Crafted with real lemons and a touch of sweetness. Refreshing. Simple. Beautiful."
+        image="/herobillede_LemonadeStand.png"
+      />
+
       <FeaturedProducts />
 
     </main>
--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,7 +1,20 @@
 import Container from "@/components/layout/Container"
 import Image from "next/image"
 
-export default function Hero() {
+type HeroProps = {
+  title: string
+  description: string
+  image: string
+  showButtons?: boolean
+}
+
+export default function Hero({
+  title,
+  description,
+  image,
+  showButtons = true
+}: HeroProps) {
+
   return (
 
     <section className="bg-heroGradient py-16 sm:py-16 lg:py-12">
@@ -14,42 +27,44 @@ export default function Hero() {
           <div className="space-y-6">
 
             <h1 className="font-heading text-heading-xl text-brand-textDark">
-              Fresh handmade lemonade
+              {title}
             </h1>
 
             <div className="w-20 h-[2px] bg-brand-primary mt-4 rounded-full"></div>
-    
+
             <p className="font-body text-body text-brand-textMuted max-w-md">
-                Crafted with real lemons and a touch of sweetness.
-                Refreshing. Simple. Beautiful.
+              {description}
             </p>
 
-            <div className="flex gap-4">
+            {showButtons && (
 
-              <button className="px-5 py-3 md:px-6 md:py-3 bg-brand-primary text-white rounded-full font-body font-medium
-                shadow-md hover:shadow-lg  hover:brightness-105
+              <div className="flex gap-4">
+
+                <button className="px-5 py-3 md:px-6 md:py-3 bg-brand-primary text-white rounded-full font-body font-medium
+                shadow-md hover:shadow-lg hover:brightness-105
                 transition-all duration-200">
                   Order Now
-              </button>
+                </button>
 
-              <button className="px-5 py-3 md:px-6 md:py-3 border border-border text-brand-textDark bg-surface-secondarybtn
+                <button className="px-5 py-3 md:px-6 md:py-3 border border-border text-brand-textDark bg-surface-secondarybtn
                 rounded-full font-body font-medium
                 hover:border-brand-primary hover:text-brand-primary
                 transition-all duration-200">
                   View Menu
-              </button>
+                </button>
 
-            </div>
+              </div>
+
+            )}
 
           </div>
-
 
           {/* Image */}
           <div className="flex justify-center md:justify-end">
 
             <Image
-              src="/herobillede_LemonadeStand.png"
-              alt="Fresh lemonade"
+              src={image}
+              alt="Hero image"
               width={520}
               height={520}
               className="w-full max-w-md md:max-w-xl lg:max-w-2xl"
