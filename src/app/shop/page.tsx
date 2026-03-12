c8565b054504964eec212afee4c2dfe33a87f16c
emmao <emf002@edu.zealand.dk>
2026-03-12 14:53:05 +0100

    Removed double grid for products

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -136,13 +136,7 @@ export default function ShopPage() {
       */}
 
       <Container>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pb-20">
-          {filtered.map(product => (
-            <ProductCard key={product.product_id} product={product} />
-          ))}
-        </div>
-
-
+       
         {/* Produktgrid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pb-20">
 
